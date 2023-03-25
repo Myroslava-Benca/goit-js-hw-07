088ff124a37f05b5ac2dc20d6fa523e09721b2b0
Myroslava-Benca <mashaivanova685@gmail.com>
2023-03-25 21:54:21 +0200

    new change

---
+++ js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery')
+
+const listGallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item"><a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a> </li>`
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', listGallery)
+
+gallery.addEventListener('click', onOpenGalleryList)
+
+function onOpenGalleryList(event) {
+    event.preventDefault();
+
+    const target = event.target;
+
+    if (target.nodeName !== 'IMG') {
+        return
+    }
+
+  const instance = basicLightbox.create(`
+    <img src="${target.dataset.source}" width="800" height="600">
+`)
+
+    instance.show()
+
+}
